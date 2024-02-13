0ec27f3cbc81a0f87bb275a5a74054102c7d3bd0
JLPlummer79 <60635785+JLPlummer79@users.noreply.github.com>
2024-02-13 15:07:37 -0800

    Add files via upload

---
+++ parse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define static instrSz = 32;    //number of bits 
+#define static opCodeSz = 7; //fix
+#define static  funct3Sz = 3;
+#define static funct7Sz = 7;
+#define static rdSz = 5; //
+#define static rsSz = 5; //
+#define static ImmI = 5; //check
+#define static ImmSz2 = 16; //check 
+
+int main() {
+
+    return 0;
+}
