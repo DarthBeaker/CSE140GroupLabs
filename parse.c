a8a7e95b6031dd82bae67c3f8751b1bddf270ece
DarthBeaker <kieranpfann@gmail.com>
2024-02-13 15:54:17 -0800

    function defs created

--- parse.c
+++ parse.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define static instrSz = 32;    //number of bits 
 #define static opCodeSz = 7; //fix
-#define static  funct3Sz = 3;
+#define static funct3Sz = 3;
 #define static funct7Sz = 7;
 #define static rdSz = 5; //
 #define static rsSz = 5; //
@@ -11,6 +11,14 @@
 #define static immSBsz5 = 5;
 #define static immSBsz = 7;
 #define static immSz2 = 16; //check 
+
+void print_instructions(char*); //main function will call subfunctions
+char parse_instructions(char*); //find the opcode, returns instruct type
+int parse_immediate(char*);
+int parse_fuct3(char*);
+int parse_fuct7(char*);
+int parse_register(char*);
+
 
 int main() {
 
