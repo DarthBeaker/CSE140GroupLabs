4c41dc76bb7e24ad049ace09916337f4ef9eb857
Jesse <bfantastic@gmail.com>
2024-02-13 16:01:46 -0800

    Updated parse.c

--- parse.c
+++ parse.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#define static instrSz = 32;    //number of bits 
-#define static opCodeSz = 7; //fix
+#include <stdio.h>                  //define the field sizes to avoid magic number JLP
+#define static instrSz = 32;    
+#define static opCodeSz = 7; 
 #define static funct3Sz = 3;
 #define static funct7Sz = 7;
-#define static rdSz = 5; //
-#define static rsSz = 5; //
-#define static immI = 5; //check
+#define static rdSz = 5; 
+#define static rsSz = 5; 
+#define static immI = 5; 
 #define static immSsz5 = 5;
 #define static immSsz7 = 7;
 #define static immSBsz5 = 5;
 #define static immSBsz = 7;
-#define static immSz2 = 16; //check 
+#define static immSz2 = 16; 
 
-void print_instructions(char*); //main function will call subfunctions
-char parse_instructions(char*); //find the opcode, returns instruct type
+void print_instructions(char*); //main function will call subfunctions KP
+char parse_instructions(char*); //find the opcode, returns instruct type KP
 int parse_immediate(char*);
 int parse_fuct3(char*);
 int parse_fuct7(char*);
