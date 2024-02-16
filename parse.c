a3243f9da50e140d3a64e9348c6c00fc0875f395
Jesse <bfantastic@gmail.com>
2024-02-16 11:23:13 -0800

    parse_instruction functioning (as expected)

--- parse.c
+++ parse.c
@@ -11,13 +11,13 @@ const int ImmI = 5;
 const int ImmSz2 = 16; 
 
 void print_instructions(char*); //main function will call subfunctions KP
-char* parse_instructions(char*); //find the opcode, returns instruct type KP
+char parse_instructions(char*); //find the opcode, returns instruct type KP
 int parse_immediate(char*);
 int parse_fuct3(char*);
 int parse_fuct7(char*);
 int parse_register(char*);
 
-char* parse_instructions(char* instr){   //since the Opcode is always the last 7 bits, we can easily extract them
+char parse_instructions(char* instr){   //since the Opcode is always the last 7 bits, we can easily extract them
     char opCode[opCodeSz];
     char R[] = "0110011";
     char I[] = "0010011";
@@ -25,29 +25,31 @@ char* parse_instructions(char* instr){   //since the Opcode is always the last 7
     char SB[] = "1100011";
     char UJ[] = "1101111";
     
-    for(int i = instrSz - opCodeSz; i < instrSz; i++) {
+    for(int i = instrSz - opCodeSz; i < instrSz; i++) { 
         opCode[i-25] = instr[i]; 
+        //printf("%i\n", i); //is iteration happening as expected? JLP
     }
 
-    printf("%s\n", opCode);
+    //printf("%s\n", opCode); // is opCode correct?
 
-    if(strcmp(opCode , R)) {
-        return "R";
+    if(strcmp(opCode , R) == 0) {            //Which type is it? JLP
+        return 'R';
     }
-    if(strcmp(opCode, I)) {
-        return "I";
+    else if(strcmp(opCode, I) == 0) {
+        return 'I';
     }
-    if(strcmp(opCode, S)){
-        return "S";
+    else if(strcmp(opCode, S) == 0){
+        return 'S';
     }
-    if(strcmp(opCode, SB)){
-        return "SB";
+    else if(strcmp(opCode, SB) == 0){
+        return 'B';                         //Note: this is our shorthand for SB JLP
     }
-    if(strcmp(opCode, UJ)){
-        return "UJ";
+    else if(strcmp(opCode, UJ) == 0){
+        return 'U';                         //Note: this is our shorthand for UJ JLP
     }
 
-    return "Invalid Code";
+    printf("%s\n", "Invalid Code");         
+     return 'Z';                            //Note: shorthand for invalid code JLP
 }
 
 
@@ -62,6 +64,7 @@ void main() {
     //echo the data for now
     printf("%s\n", instr);
 
-    printf("%s\n",parse_instructions(instr));
+    //call parse_instruction print return symbol for now.
+    printf("%c\n",parse_instructions(instr));
     
 }
