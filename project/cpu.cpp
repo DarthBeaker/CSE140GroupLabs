f1c45d08f2a9e1e80e946e39f51c65653b778d61
Jesse <bfantastic@gmail.com>
2024-03-22 14:48:35 -0700

    Added Read_rf to Decode, will need to modify for correctness code flow, correctness

--- project/cpu.cpp
+++ project/cpu.cpp
@@ -15,40 +15,43 @@ Cpu::~Cpu(){
 //simplistic version of Reading the register file.
 //arg is the desired register; it returns the int...
 
- int Cpu::Read_rf(int ptr) {
-
+int Cpu::Read_rf(int ptr) {
+    
     return rf[ptr];
 
 }
 
 Cpu::Decode(const char* instr) {  //this is the rf call
     char op;
-    int funct_3, funct_7, imme, rd, rs1, rs2;
+    int instr, funct_3, funct_7, imme, rd, rs1, rs2;
+
     //will need to call Read_rf and pass to parse...
+    //fetch each instruction sequentially 0 - 32
+    for(int i = 0; i < 32; i++) {
+        instr = self.Read_rf(i);
+        op = parse_instructions(instr);
+        funct_3 = parse_funct3;
+        funct_7 = parse_funct7;
+        imme = parse_immediate(instr);
+        //borrowed from parse register function that chooses the registers?
+        if(op == 'R' || op == 'J' || op == 'I') {
+            rd = sub_parse_reg_rd(instr);
 
-    op = parse_instructions(instr);
-    funct_3 = parse_funct3;
-    funct_7 = parse_funct7;
-    imme = parse_immediate(instr);
-    //borrowed from parse register function that chooses the registers?
-    if(op == 'R' || op == 'J' || op == 'I') {
-        rd = sub_parse_reg_rd(instr);
-
-        if(op == 'R' || op == 'I'){
-            
-            rs1 = sub_parse_reg_rs1(instr);
-            if(op == 'R'){
-                rs2 = sub_parse_reg_rs2(instr);
+            if(op == 'R' || op == 'I'){
+                
+                rs1 = sub_parse_reg_rs1(instr);
+                if(op == 'R'){
+                    rs2 = sub_parse_reg_rs2(instr);
+                }
             }
         }
-    }
-    else if(op == 'S' || op == 'B') {
-        int rs1 = sub_parse_reg_rs1(instr);
-        //printf("Rs1: x%i \n", rs1);
-        int rs2 = sub_parse_reg_rs2(instr);
+        else if(op == 'S' || op == 'B') {
+            int rs1 = sub_parse_reg_rs1(instr);
+            //printf("Rs1: x%i \n", rs1);
+            int rs2 = sub_parse_reg_rs2(instr);
         //printf("Rs2: x%i \n", rs2);
+        }
     }
-
     //rd = sub_parse_reg_rd(instr);
     //rs1 = sub_parse_reg_rs1(instr);
     //rs2 = sub_parse_reg_rs2(instr);
